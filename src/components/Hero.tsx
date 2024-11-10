82122d844d63f6d3c6c68dc01b63644d4531fee8
Tanveerkumar12 <165558620+Tanveerkumar12@users.noreply.github.com>
2024-11-10 12:22:05 +0500

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,7 +4,7 @@ import Navbar from './Navbar'
 const Hero = () => {
   return (
     <div id="hero" className="min-h-screen bg-no-repeat bg-[url(/banner_bg.png)] bg-cover"
-    style={{backgroundSize: "35%", backgroundPosition: "left 100px lop 100px"}}
+    style={{backgroundSize: "35%", backgroundPosition: "left 100px lop 100px" , margin:"20px"}}
     >
       <Navbar />
       <div className='container grid lg:grid-cols-2 h-[cals(100vh-60px)]'>
