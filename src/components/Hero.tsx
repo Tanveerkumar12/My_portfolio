40289f53e2ed16dc9cff21ae8c84333def836bb3
Tanveerkumar12 <tanveerkumar787@gmail.com>
2024-11-10 10:27:51 +0500

    first commit

---
+++ src/components/Hero.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const Hero = () => {
+  return (
+    <div id="hero" className="min-h-screen bg-no-repeat bg-[url(/banner_bg.png)] bg-cover"
+    style={{backgroundSize: "35%", backgroundPosition: "left 100px lop 100px"}}
+    >
+      <Navbar />
+      <div className='container grid lg:grid-cols-2 h-[cals(100vh-60px)]'>
+        <div className='hidden lg:block'></div>
+        <div className='text-[80px] sm:text-[100px] font-bold leading-tight flex justify-center items-center'>
+         <div> <p data-aos="zoom-in-up">I am</p>
+          <p data-aos="zoom-in-up">Tanveer</p>
+          </div>
+        </div>
+      </div>
+      
+    </div>
+  )
+}
+
+export default Hero
---
+++ src/components/Projects.tsx
@@ -0,0 +1,42 @@
+import { title } from 'process'
+import React from 'react';
+import Heading from './Heading';
+import Card from './Card';
+
+const data =[
+    {
+        id: 0,
+        title: " Simple Calculator ",
+        desc: "Created a simple calculator using Typescript, Node.js and Inquirer",
+        img: "/Project_01.png",
+        tags: ["HTML", "Node","CSS","Typescript"],
+
+    },
+    {
+      id: 1,
+      title: " Dynomic routing ",
+      desc: " created a dynomic route for displaying country details ",
+      img: "/Project_02.png",
+      tags: ["HTML","CSS", "Typescript","Javascript"],
+    },
+]
+
+const Projects = () => {
+  return (
+    <div id='projects' className='container pt-32'>
+      <Heading title='My Projects' />
+      <div className='grid gap-10 xl:gap-0 xl:gap-y-10 md:grid-cols-2 lg:grid-cols-3 place-items-center'>
+        {data.map((el) => (<Card 
+        key={el.id}
+        title={el.title}
+        desc={el.desc}
+        img={el.img}
+        tags={el.tags}
+        />))}
+      </div>
+      
+    </div>
+  )
+}
+
+export default Projects
